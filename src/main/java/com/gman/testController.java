e03a128e4067f3258603bb030c888a6dd14e40e4
gmanpark <gmanpark@tmon.co.kr>
2015-11-10 22:52:54 +0900

    [TASK] spring boot project

---
+++ src/main/java/com/gman/testController.java
@@ -0,0 +1,14 @@
+package com.gman;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @RequestMapping("/test")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "HELLO";
+    }
+}
