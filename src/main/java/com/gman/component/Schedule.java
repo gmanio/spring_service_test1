70c94da5a8b668d0c8d2e17b46262789cc3a0577
gman <gmanpark@tmon.co.kr>
2015-11-27 16:14:25 +0900

    [TASK] error page 처리

--- src/main/java/com/gman/component/Schedule.java
+++ src/main/java/com/gman/component/Schedule.java
@@ -2,17 +2,22 @@ package com.gman.component;
 
 import com.gman.repository.User;
 import com.gman.service.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
 
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     UserServiceImpl userServiceImpl;
 
-    @Scheduled(fixedRate=5000)
+//    @Scheduled(fixedRate=5000)
     public void insert() {
         User saveUser = new User();
         saveUser.setName("PPPP");
@@ -20,5 +25,13 @@ public class Schedule {
         saveUser.setAddress("AAAA");
 
         userServiceImpl.create(saveUser);
+    }
+
+    @Value("${gman.name}")
+    String name;
+
+    @Scheduled(fixedRate=5000)
+    public void printProperty(){
+        logger.debug(name);
     }
 }
--- src/main/java/com/gman/controller/ServiceController.java
+++ src/main/java/com/gman/controller/ServiceController.java
@@ -1,19 +1,40 @@
 package com.gman.controller;
 
+import com.gman.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.SimpleDateFormat;
 
 /**
  * Created by gmanpark on 2015-11-18.
  */
 @Controller
-@RequestMapping("/service")
 public class ServiceController {
-    @RequestMapping("/index")
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    UserService userRepository;
+
+    @RequestMapping(value="/index", method = RequestMethod.GET)
     public String index(Model model) {
 
+        model.addAttribute("userList", userRepository.findAll());
 
-        return "/index";
+        return "index";
+    }
+
+    @RequestMapping(value="/date", method = RequestMethod.GET)
+    public SimpleDateFormat date(Model model) {
+        String format = "mm/dd/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+        logger.info(String.valueOf(sdf));
+        return sdf;
     }
 }
